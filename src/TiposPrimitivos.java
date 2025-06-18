b5e283ed3930e0d2e75c76ee15714a5730e2fa6c
Vitor Kravszenko <vitor.kravs@accist.com.br>
2025-06-17 23:02:39 -0300

    feat: estudo dos tipos primitivos

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        byte number = 1;
+        short numberShort = 1;
+        int numberInt = 1;
+        long numberLong = 1L;
+        var numberLongTwo = 1L;
+        float numberFloat = 1f;
+        var numberFloatTwo = 1f;
+        double numberDouble = 1.0;
+        var numberDoubleTwo = 1.0d;
+        char character = 'a';
+        boolean bool = true;
+    }
+
+}
